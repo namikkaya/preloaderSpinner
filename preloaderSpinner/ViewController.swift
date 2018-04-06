c06e6e52087e3e6c1e8d83e1a439312c6260d585
namikkaya <namikkaya86@gmail.com>
2018-04-06 21:30:41 +0300

    Initial Commit

---
+++ preloaderSpinner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  preloaderSpinner
+//
+//  Created by namik kaya on 6.04.2018.
+//  Copyright © 2018 namik kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
